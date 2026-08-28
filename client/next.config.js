eb210a1d794c0bb18bd94da8bba0bac18954ecca
KAUSHIK6667 <kaushikterli6@gmail.com>
2026-08-28 23:13:30 +0530

    Fix Vercel Next.js output directory

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	distDir: process.env.NODE_ENV === 'production' ? '.next-prod' : '.next'
+	 reactStrictMode: true
 };
 module.exports = nextConfig;
